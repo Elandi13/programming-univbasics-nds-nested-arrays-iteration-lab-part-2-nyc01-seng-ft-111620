ebd9d5bcc389d34976b67504de69569085aa2b68
Elvis Landi <elandi13@yahoo.com>
2020-11-07 16:50:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ while row_index < src.count do
   element_index = 0
   inner_results = []
   lowest_value = 100
-  while element_index <src[row_index].count do
+  while element_index < src[row_index].count do
     if src[row_index][element_index] < lowest_value
       lowest_value = src[row_index][element_index]
     end
